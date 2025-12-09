6379b621331fa859cc47d6adebeb72d7ff15ed69
a4koshin <mankajr11@gmail.com>
2025-12-09 17:31:30 +0300

    unit is changed

--- backend/models/inventory/unit.js
+++ backend/models/inventory/unit.js
@@ -7,6 +7,7 @@ const unitSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "Provider",
     },
+    abbreviation: String,
     status: {
       type: Number,
       enum: [0, 1, 2],
